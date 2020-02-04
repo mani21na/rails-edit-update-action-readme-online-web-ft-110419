57adb0a22045f6f96845049ce8e0aec46819df89
Hanna Lee <mani21na@gmail.com>
2020-02-04 00:43:49 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,9 +21,9 @@ class ArticlesController < ApplicationController
 
   # add edit and update methods here
   def edit
-    @articlae = Article.find(params[:id])
+    @article = Article.find(params[:id])
   end
-  
+
   def update
     @article = Article.find(params[:id])
     @article.update(title: params[:article][:title], description: params[:article][:description])
